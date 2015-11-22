218428d32fc5b7e69ef7be794218a8cd24de97af
chexee <chelsea@vouch.com>
2015-11-21 23:26:36 -0600

    Spell ui.router correctly. Add templates.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,29 @@
 // Declare Angular module
-var blocJams = angular.module('blocJams', ['ui-router']);
+var blocJams = angular.module('blocJams', ['ui.router']);
 
-blocJams.config( function ( $stateProvider, $locationProvider ) {
-  $locationProvider.html5mode({
+blocJams.config( function($stateProvider, $locationProvider) {
+  $locationProvider.html5Mode({
     enabled: true,
     requireBase: false
   });
 
   // Add album state.
   // Pass in state name and config options.
-  $stateProvider.state('album', {
-    url: '/album',
-    controller: 'Album.controller',
-    templateUrl: '/templates/album.html'
-  });
+  $stateProvider
+    .state('landing', {
+      url: '/landing',
+      controller: 'Landing.controller',
+      templateUrl: '/templates/landing.html'
+    })
+    .state('collection', {
+      url: '/collection',
+      controller: 'Collection.controller',
+      templateUrl: '/templates/collection.html'
+    })
+    .state('album', {
+      url: '/album',
+      controller: 'Album.controller',
+      templateUrl: '/templates/album.html'
+    })
+
 });
