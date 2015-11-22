92c988ae6eb57b6346d1170ed9a5425a4db2dcd2
chexee <chelsea@vouch.com>
2015-11-21 23:02:15 -0600

    Add album state

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,2 +1,17 @@
 // Declare Angular module
 var blocJams = angular.module('blocJams', ['ui-router']);
+
+blocJams.config( function ( $stateProvider, $locationProvider ) {
+  $locationProvider.html5mode({
+    enabled: true,
+    requireBase: false
+  });
+
+  // Add album state.
+  // Pass in state name and config options.
+  $stateProvider.state('album', {
+    url: '/album',
+    controller: 'Album.controller',
+    templateUrl: '/templates/album.html'
+  });
+});
