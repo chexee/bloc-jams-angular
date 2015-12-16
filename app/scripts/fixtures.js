1cbeca3265b4da66cb45774ad9af56907304d65f
chexee <chelsea@vouch.com>
2015-12-16 00:02:32 -0800

    Update to accurate lengths

--- app/scripts/fixtures.js
+++ app/scripts/fixtures.js
@@ -5,11 +5,11 @@ var albumPicasso = {
   year: '1881',
   albumArtUrl: '/assets/images/album_covers/01.png',
   songs: [
-    { name: 'Blue', length: '4:26', audioUrl: '/assets/music/blue' },
-    { name: 'Green', length: '3:14', audioUrl: '/assets/music/green' },
-    { name: 'Red', length: '5:01', audioUrl: '/assets/music/red' },
-    { name: 'Pink', length: '3:21', audioUrl: '/assets/music/pink' },
-    { name: 'Magenta', length: '2:15', audioUrl: '/assets/music/magenta' }
+    { name: 'Blue', length: '3:42', audioUrl: '/assets/music/blue' },
+    { name: 'Green', length: '2:44', audioUrl: '/assets/music/green' },
+    { name: 'Red', length: '4:28', audioUrl: '/assets/music/red' },
+    { name: 'Pink', length: '3:33', audioUrl: '/assets/music/pink' },
+    { name: 'Magenta', length: '6:14', audioUrl: '/assets/music/magenta' }
   ]
 }
 
@@ -20,11 +20,11 @@ var albumMarconi = {
   year: '1909',
   albumArtUrl: '/assets/images/album_covers/20.png',
   songs: [
-    { name: 'Hello, Operator?', length: '1:01', audioUrl: '/assets/music/blue' },
-    { name: 'Ring, ring, ring', length: '5:01', audioUrl: '/assets/music/green' },
-    { name: 'Fits in your pocket', length: '3:21', audioUrl: '/assets/music/red' },
-    { name: 'Can you hear me now?', length: '3:14', audioUrl: '/assets/music/pink' },
-    { name: 'Wrong phone number', length: '2:15', audioUrl: '/assets/music/magenta' }
+    { name: 'Hello, Operator?', length: '3:42', audioUrl: '/assets/music/blue' },
+    { name: 'Ring, ring, ring', length: '2:44', audioUrl: '/assets/music/green' },
+    { name: 'Fits in your pocket', length: '4:28', audioUrl: '/assets/music/red' },
+    { name: 'Can you hear me now?', length: '3:33', audioUrl: '/assets/music/pink' },
+    { name: 'Wrong phone number', length: '6:45', audioUrl: '/assets/music/magenta' }
   ]
 }
 
@@ -35,10 +35,10 @@ var albumRothko = {
   year: '1949',
   albumArtUrl: '/assets/images/album_covers/04.png',
   songs: [
-    { name: 'Rust and orange', length: '2:31', audioUrl: '/assets/music/blue' },
-    { name: 'Magenta, Black, Green on Orange', length: '3:41', audioUrl: '/assets/music/green' },
-    { name: 'Four darks in red', length: '1:27', audioUrl: '/assets/music/red' },
-    { name: 'Black on Grey', length: '2:16', audioUrl: '/assets/music/pink' },
-    { name: 'White center', length: '3:24', audioUrl: '/assets/music/magenta' }
+    { name: 'Rust and orange', length: '3:42', audioUrl: '/assets/music/blue' },
+    { name: 'Magenta, Black, Green on Orange', length: '2:44', audioUrl: '/assets/music/green' },
+    { name: 'Four darks in red', length: '4:28', audioUrl: '/assets/music/red' },
+    { name: 'Black on Grey', length: '3:33', audioUrl: '/assets/music/pink' },
+    { name: 'White center', length: '6:14', audioUrl: '/assets/music/magenta' }
   ]
 }
